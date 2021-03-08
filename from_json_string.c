4f28bed351121d168eb94154c8b61e9e663c6fb7
João Pedro Fonseca Dantas <67479090+jopefd@users.noreply.github.com>
2021-03-08 13:17:58 -0300

    Add main's scope and function format

--- from_json_string.c
+++ from_json_string.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+void format(char *string) {
+  char *num = strtok(string + 1, ",");
+
+  int i = 0;
+  while (num != NULL) {
+    string[i] = *num;
+    num = strtok(NULL, ", ");
+    i++;
+  }
+}
+
+int verify_doubles() {
+
+  return;
+}
+
 int *from_json_string_int(char string[201], int *ptrtam) {
   int *vetor_int = malloc(strlen(string) * sizeof(int));
 
@@ -27,7 +43,15 @@ double *from_json_string_double(char string[201], int *ptrtam) {
 }
 
 int main(void) {
-  
+  int n;
+
+  scanf("%i", &n);
+
+  char *json_arrays = malloc(n * sizeof(char));
+  for (int i = 0; i < n; i++) {
+    fgets(json_arrays[i], 202, );
+     
+  }
 
   return 0;
 }

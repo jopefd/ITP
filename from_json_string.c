1b859ead745d1d6e0bd33525688e98a0fc5cbaa7
João Pedro Fonseca Dantas <67479090+jopefd@users.noreply.github.com>
2021-03-09 12:46:16 -0300

    Fix function format

--- from_json_string.c
+++ from_json_string.c
@@ -2,27 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void format(char *string) {
-  char *num = strtok(string + 1, ", ]");
-  
-  int i = 0;
-
-  while (num != NULL) {
-    if (strlen(num) >= 3) {
-      for (int j = 0; j < strlen(num); j++) {
-        string[i + j] = num[j];
-      }      
-      i = i + strlen(num) - 1;
-    } else {
-      string[i] = *num;
-    }    
-    num = strtok(NULL, ", ]");    
-    i++;
-  }
-
-  string[i] = '\0';
-}
-
 int check_doubles(char *string) {
   for (int i = 0; i < strlen(string); i++) {
     if (string[i] == '.')
@@ -32,16 +11,22 @@ int check_doubles(char *string) {
   return 0;
 }
 
-int *from_json_string_int(char string[201], int *ptrtam) {
-  int *vetor_int = malloc(strlen(string) * sizeof(int));
+char** format(char *array_json) {
+  char** string_only_numbers;
+  char *number = strtok(array_json + 1, ", ]");
+  int i = 0;
 
-  for (int i = 0; i < strlen(string); i++) {
-    vetor_int[i] = string[i] - '0';
+  string_only_numbers = malloc(strlen(array_json) * sizeof(char*));
+
+  while (number != NULL) {
+    string_only_numbers[i] = number;
+    number = strtok(NULL, ", ]");
+    i++;
   }
 
-  *ptrtam = strlen(string);
-
-  return vetor_int;
+  string_only_numbers = realloc(string_only_numbers, (i - 1) * sizeof(char*));
+  
+  return string_only_numbers;
 }
 
 double *from_json_string_double(char string[201], int *ptrtam) {
@@ -56,32 +41,49 @@ double *from_json_string_double(char string[201], int *ptrtam) {
   return vetor_double;
 }
 
-int main(void) {
-  int n;
+int *from_json_string_int(char string[201], int *ptrtam) {
+  int *vetor_int = malloc(strlen(string) * sizeof(int));
 
+  for (int i = 0; i < strlen(string); i++) {
+    vetor_int[i] = string[i] - '0';
+  }
+
+  *ptrtam = strlen(string);
+
+  return vetor_int;
+}
+
+int main(void) {
+  // Ler um número N
+  int n, tam;
   scanf("%i\n", &n);
 
+  // Laço de N repetições
   for (int i = 0; i < n; i++) {
+    // Ler array JSON
     char json_array[202];
-
     fgets(json_array, 202, stdin);
-    printf("oi");
     json_array[strlen(json_array) - 1] = '\0';
+    json_array = format()
 
-    format(json_array);
-
-    int *ptrtam;
-
+    // Converter array JSON
+    // Verificar se tem doubles
     if (check_doubles(json_array) == 1) {
-      double *ptrd = from_json_string_double(json_array, ptrtam);
-      printf("%p", ptrd);
-      free(ptrd);
+      double *ptrd = from_json_string_double(json_array, &tam);
     } else {
-      int *ptri = from_json_string_int(json_array, ptrtam);
+      int *ptri = from_json_string_int(json_array, &tam);
       printf("%i", *ptri);
       free(ptri);
     }
+        // Se sim tem, criar vetor de doubles
+
+        // Se não tem, criar vetor de inteiros
+
+    int *ptrtam;
+
   }
+    // Imprimir array convertido
+
 
   return 0;
 }

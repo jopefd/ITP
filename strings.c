77f3633d9f0e5854f6942293bf407f85eda1ba71
João Pedro Fonseca Dantas <fonsecajope@gmail.com>
2021-03-13 19:52:40 -0300

    Fix calc of string capacity

--- strings.c
+++ strings.c
@@ -10,10 +10,12 @@ typedef struct {
 
 void set_str(string *text, char *char_array) {
   if (strlen(char_array) % 16 > 0) {
-    text->string_capacity = 16 - (strlen(char_array) % 16);
+    text->string_capacity = 16 + strlen(char_array) - strlen(char_array) % 16;
   } else {
     text->string_capacity = strlen(char_array);
   }
+
+  printf("%u", text->string_capacity);
 
   text->array = malloc(text->string_capacity * sizeof(char));
 }
@@ -23,27 +25,31 @@ void read_str(string *text) {
   text->array[strlen(text->array) - 1] = '\0';
 }
 
-void concat_str(char *string_1, char *string_2) {
-  strcat(string_1, string_2);
+void concat_str(string *string_1, string *string_2) {
+  strcat(string_1->array, string_2->array);
 }
 
-void clear_str(char *string) {
-  free(string);
+void clear_str(string *string) {
+  free(string->array);
+  string->string_capacity = 0;
+  string->string_lenght = 0;
 }
 
 int main(void) {
   string greeting;
   string name;
 
-  set_str(&greeting, "Oi ");
-  read_str(&name);
-  concat_str(&greeting, &name);
+  set_str(&greeting, "Oisjsjsjsjsjsjsjsj ");
+  printf("oif");
+  printf("%s", greeting.array);
+  //read_str(&name);
+//  concat_str(&greeting, &name);
 
-  printf("%s\n", greeting.str);
-  printf("%i %i\n", greeting.capacity, greeting.length);
+//  printf("%s\n", greeting.array);
+//  printf("%i %i\n", greeting.string_capacity, greeting.string_lenght);
   
-  clear_str(&greeting);
-  clear_str(&name);
+//  clear_str(&greeting);
+//  clear_str(&name);
   
   return 0;
 }

07dc6c15f50ed81cc2bff1541b1ed267eee23a07
João Pedro Fonseca Dantas <fonsecajope@gmail.com>
2021-03-14 21:56:31 -0300

    Rename variables and add some tests

--- strings.c
+++ strings.c
@@ -9,10 +9,12 @@ typedef struct {
 } string;
 
 void set_str(string *text, char *char_array) {
+  text->length = strlen(char_array);
+
   if (strlen(char_array) % 16 > 0) {
-    text->capacity = 16 + strlen(char_array) - strlen(char_array) % 16;
+    text->capacity = 16 + text->length - text->length % 16;
   } else {
-    text->capacity = strlen(char_array);
+    text->capacity = text->length; 
   }
 
   text->array = malloc(text->capacity * sizeof(char));
@@ -21,6 +23,7 @@ void set_str(string *text, char *char_array) {
 
 void read_str(string *text) {
   int counter = 0;
+
   do {
     counter++;
     text->capacity = 16 * counter;
@@ -28,17 +31,19 @@ void read_str(string *text) {
     if (counter == 1) {
       text->array = malloc(text->capacity * sizeof(char));
     } else {
-      text->array = realloc(text->array, text->capacity);
+      text->array = realloc(text->array, text->capacity * sizeof(char));
     }
 
-    text->array = malloc(text->capacity * sizeof(char));
     fgets(text->array, text->capacity, stdin);
-    printf("oi");
-  } while (text->array[strlen(text->array) - 1] != '\n');
+    text->length = strlen(text->array);
+  } while (text->array[text->length - 1] != '\n');
+
+  text->array[text->length - 1] = '\0';
 }
 
 void concat_str(string *text_1, string *text_2) {
   strcat(text_1->array, text_2->array);
+  text_1->length = strlen(text_1->array);
 }
 
 void clear_str(string *string) {
@@ -51,15 +56,15 @@ int main(void) {
   string greeting;
   string name;
 
-  set_str(&greeting, "Oisjsjsjsjsjsjsjsj ");
+  set_str(&greeting, "Oi ");
   read_str(&name);
-//  concat_str(&greeting, &name);
+  concat_str(&greeting, &name);
 
-//  printf("%s\n", greeting.array);
-//  printf("%i %i\n", greeting.capacity, greeting.length);
+  printf("%s\n", greeting.array);
+  printf("%i %i\n", greeting.capacity, greeting.length);
   
-//  clear_str(&greeting);
-//  clear_str(&name);
+  clear_str(&greeting);
+  clear_str(&name);
   
   return 0;
 }

f19ec4ad5e3567041c66e84d7bb357387155dc8f
João Pedro Fonseca Dantas <fonsecajope@gmail.com>
2021-03-17 19:40:37 -0300

    Fix functions read_teams_results

--- bola_pro_mato.c
+++ bola_pro_mato.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
   char name[202];
-  unsigned int points;
-  unsigned int matches;
-  unsigned int victories;
-  unsigned int ties;
-  unsigned int defeats;
-  unsigned int home_goals;
-  unsigned int away_goals;
+  int points;
+  int matches;
+  int victories;
+  int ties;
+  int defeats;
+  int home_goals;
+  int away_goals;
   int goal_difference;
 } team;
 
+void clear_buffer() {
+  while (getchar() != '\n') {
+
+  }
+}
+
 void read_teams_results(team *some_team) {
-  unsigned int input_capacity;
+  int input_capacity;
   char *input;
   char *token;
-
+  
   input_capacity = 202; 
-  input = malloc(input_capacity * sizeof(char));
-  while (fgets(input, input_capacity, stdin) != NULL) {
-    input_capacity++;
-    input = realloc(input, input_capacity * sizeof(char));
-  }
+  input = realloc(input, input_capacity * sizeof(char));
+  fgets(input, input_capacity, stdin);
 
-  strcpy(some_team, strtok(input, ";"));
-  some_team->victories = (unsigned int)atoi(strtok(NULL, " "));
+  strcpy(some_team->name, strtok(input, ";"));
+  some_team->victories = atoi(strtok(NULL, " "));
   some_team->ties = (unsigned int)atoi(strtok(NULL, " "));
   some_team->defeats = (unsigned int)atoi(strtok(NULL, " "));
   some_team->home_goals = (unsigned int)atoi(strtok(NULL, " "));
@@ -34,31 +38,34 @@ void read_teams_results(team *some_team) {
 }
 
 int main(void) {
-  unsigned int teams_quantity;
+  int teams_quantity;
   team cartola[teams_quantity];
   team ranking[teams_quantity];
-
-  scanf("%u", &teams_quantity);
+  
+  scanf("%i\n", &teams_quantity);
   for (int i = 0; i < teams_quantity; i++) {
     read_teams_results(&cartola[i]);
   }
 
-  for (int i = 0; i < teams_quantity; i++) {
-    ranking[i] = cartola[i];
-    for (int j = 0; j < teams_quantity; j++) {
-      if (cartola[j].points > ranking[i].points) {
-        ranking[i] = cartola[j];
-        continue;
-      }
-    }
-  }
  
-  printf("Nome| Pontos| Jogos| ");
-  printf("Vitorias| Empates| Derrotas| ");
-  printf("Gols Pro| Gols Contra| Saldo de Gols");
-  for (int i = 0; i < teams_quantity; i++) {
-    printf("%s| %u| %u| ", ranking[i].name, ranking[i].points, ranking[i].matches);
-    printf("%u| %u| %u| ", ranking[i].victories, ranking[i].ties, ranking[i].defeats);
-    printf("%u| %u| %u|", ranking[i].home_goals, ranking[i].away_goals, ranking[i].goal_difference);
-  }
+//   team changer;
+//   for (int i = 0; i < teams_quantity; i++) {
+//     for (int j = i; j < teams_quantity; j++) {
+//       if (cartola[i].points < cartola[j].points) {
+//         changer = cartola[i];
+//         cartola[i] = cartola[j];
+//         cartola[j] = changer;
+//       }
+//     }
+//   }
+ 
+//   printf("Nome| Pontos| Jogos| ");
+//   printf("Vitorias| Empates| Derrotas| ");
+//   printf("Gols Pro| Gols Contra| Saldo de Gols");
+//   for (int i = 0; i < teams_quantity; i++) {
+//     printf("%s| %u| %u| ", ranking[i].name, ranking[i].points, ranking[i].matches);
+//     printf("%u| %u| %u| ", ranking[i].victories, ranking[i].ties, ranking[i].defeats);
+//     printf("%u| %u| %u|\n", ranking[i].home_goals, ranking[i].away_goals, ranking[i].goal_difference);
+//   }
 }
+
